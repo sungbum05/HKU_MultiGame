111898f2df4835732f85e02f9151c552d079c99c
sungbum05 <sblim05@naver.com>
2022-12-13 23:48:22 +0900

    NewLobbyScene

--- HKU_MultiGame/Assets/Script/PlayerInfo.cs
+++ HKU_MultiGame/Assets/Script/PlayerInfo.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 
 public class PlayerInfo : MonoBehaviourPun
 {
     [SerializeField]
     private TextMeshProUGUI PlayerName;
+    [SerializeField]
+    private Player[] players;
 
     // Start is called before the first frame update
     void Start()
